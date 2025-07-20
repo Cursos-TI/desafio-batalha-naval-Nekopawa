8f1f6a64f14bb9a4a8f0f95ddb2de4210e616b9b
Nekopawa <rafaalinehoffman@gmail.com>
2025-07-20 10:10:39 -0300

    Desafio novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,10 +6,105 @@
 
 int main() {
     // Nível Novato - Posicionamento dos Navios
+    
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int tabuleiro[10][10];
+    char verticalIndex = 'A';
+    int horizontalIndex = 0;
 
+    //preenche cada posição do tabuleiro com 0
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            tabuleiro[i][j] = 0;
+        }        
+    }
+
+    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
+    //navio vertical
+    int navio1[3][2] = {
+        {1, 2},
+        {2, 2},
+        {3, 2}
+    };
+    
+    //navio horizontal
+    int navio2[3][2] = {
+        {5, 4},
+        {5, 5},
+        {5, 6}
+    };
+
+    //mudar posição do tabuleiro para 3 quando tem um navio
+    //navio 1
+    for (int i = 0; i < 3; i++) {
+        int navioX = navio1[i][0];
+        int navioY = navio1[i][1];
+
+        //valida coordenadas do navio
+        if ((navioX >= 0 && navioY <= 10) && (navioY >= 0 && navioY <= 10)) {
+            tabuleiro[navioX][navioY] = 3;
+        } else {
+            printf("\nCoordenadas do navio 1 inválidas");
+            return 0;
+        }
+    }
+    
+    //navio 2
+    for (int i = 0; i < 3; i++) {
+        int navioX = navio2[i][0];
+        int navioY = navio2[i][1];
+
+        //evitar sobreposição
+        if (tabuleiro[navioX][navioY] == 0) {
+            //valida coordenadas do navio
+            if ((navioX >= 0 && navioY <= 10) && (navioY >= 0 && navioY <= 10)) {
+                tabuleiro[navioX][navioY] = 3;
+            } else {
+                printf("\nCoordenadas do navio 2 inválidas");
+                return 0;
+            }
+            tabuleiro[navioX][navioY] = 3;
+        } else {
+            printf("Sobreposição de navios.\n");
+            return 0;
+        }
+    }
+
+    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    printf("\n");
+
+    for (int i = 0; i < 10; i++) {
+
+        //imprimir as letras das posições verticais (A - J)
+        if (i == 0) {
+            //adiciona três caracteres de espaço no começo da linha, para ficarem alinhados
+            printf("%*c", 3, ' ');
+            for (int k = 0; k < 10; k++, verticalIndex++) {
+                printf("%c ", (char) verticalIndex);
+            }            
+            printf("\n");
+        }
+
+        //imprimir o valor de cada posição do tabuleiro
+        //acessar cada posição dentro da linha horizontal
+        for (int j = 0; j < 10; j++) {
+            
+            //imprimir índice das posições horizontais (1 - 10) na primeira coluna
+            if (j == 0) {
+                //adiciona um caractere de espaço antes do índice, exceto 10, para ficarem alinhados
+                if (i < 9) {
+                    printf("%*c", 1, ' ');
+                }
+                printf("%d ", ++horizontalIndex);
+            }
+
+            //imprimir valor da posição no tabuleiro
+            printf("%d ", tabuleiro[i][j]);
+        } 
+
+        printf("\n");  
+    }
+    
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
